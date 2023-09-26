fc291a3f556379f617c8895ab1e9682d0ab8df71
prabhatparida <137138319+prabhatparida@users.noreply.github.com>
2023-09-26 03:36:57 +0000

    OPERATION

--- 01_conversionOperation.js
+++ 01_conversionOperation.js
@@ -23,6 +23,52 @@ let booleanIsLoggedIn = Boolean(isLoggedIn);
 let someNumber = 33;
 
 let StringNumber = String(someNumber);
- console.log( typeof StringNumber);
+ // console.log( typeof StringNumber);
+
+ // ***************** Operations ******************
+
+let value = 3;
+let negValue = -value;
+// console.log(negValue);
+
+let str = " Prabhat";
+let str1 = " Parida";
+ 
+let str3 = str + str1;
+
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2" );
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3;
+
+num1= num2 = num3 = 2 + 2;
+
+let gameCounter = 100;
+gameCounter++;
+// console.log(gameCounter);
+
+// EXAMPLE OF POSTFIX AND PREFIX
+
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
+
+
+
+
 
 
