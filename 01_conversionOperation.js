b6141bd73631a7967edc119c28b421b1351fd94b
prabhatparida <137138319+prabhatparida@users.noreply.github.com>
2023-09-26 03:07:25 +0000

    conversion of datatypes

---
+++ 01_conversionOperation.js
@@ -0,0 +1,28 @@
+let age = "24";
+
+// console.log( typeof age);
+// console.log(typeof (age));
+
+
+let valueInNumber = Number(age);
+
+// console.log(typeof valueInNumber);
+
+//" 24" => 24
+// "324asdf" => NaN
+// true => 1; false => 0;
+
+let isLoggedIn = "";
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0=> false;
+// "" => false
+// "Prabhat" => true;
+
+let someNumber = 33;
+
+let StringNumber = String(someNumber);
+ console.log( typeof StringNumber);
+
+
