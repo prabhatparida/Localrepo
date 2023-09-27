13a55d5ef945fe6b9a321d8beec4b9398f2de5c1
prabhatparida <137138319+prabhatparida@users.noreply.github.com>
2023-09-27 03:40:17 +0000

    basic datatypes

---
+++ 01_basicdatatypes.js
@@ -0,0 +1,51 @@
+// Primitive
+
+// 7 categories: String, Number, Boolean, null,undefined,Symbol, BigInt
+
+const score =100;
+const scoreValue = 100.3;
+const isLoggedin =false;
+
+const outsideTemp = null;
+let userEmail;
+
+const id = Symbol('123');
+const anotherId  = Symbol('123');
+
+// console.log(id === anotherId);
+
+const bigNumber = 22323121123232734444332n;
+
+
+
+// Reference Type( Non Primitive)
+
+// Arrays, Objects, Functions
+
+// Arrays
+const heros = ['Srikrushna','Ramchandra', 'Arjun','Shiva', 'Ganesh'];
+// console.table(heros);
+
+// Objects
+
+let personalId = {
+    name: "Prabhat",
+    age: 24,
+    employeeId: 180,
+
+};
+
+// console.log(... personalId.name);
+
+
+// function
+
+const myFunction = function(){
+   //  console.log(" Hello Prabhat");
+}
+
+console.log(typeof outsideTemp);
+
+
+
+
