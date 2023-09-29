9f57a192d0ccc49c441f5394c075b35a2550522d
prabhatparida <137138319+prabhatparida@users.noreply.github.com>
2023-09-29 03:25:20 +0000

    Numbers

---
+++ 01_numsmaths.js
@@ -0,0 +1,23 @@
+const score = 300;
+
+// console.log(score);
+
+const balance = new Number(120);
+// console.log(balance);
+
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const otherNumber = 23.56747
+
+// console.log(otherNumber.toPrecision(3));
+
+const hundreds = 1000000;
+
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// ++++++++++ MATHS+++++++++++++++++++++
+
+console.log(Math);
+console.log(Ma)
