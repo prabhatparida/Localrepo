9ebbb350f77425899b5d389d8ced0d78669456d8
prabhatparida <137138319+prabhatparida@users.noreply.github.com>
2023-09-29 03:51:28 +0000

    Maths

--- 01_numsmaths.js
+++ 01_numsmaths.js
@@ -17,7 +17,32 @@ const hundreds = 1000000;
 
 // console.log(hundreds.toLocaleString('en-IN'));
 
-// ++++++++++ MATHS+++++++++++++++++++++
+// ++++++++++ MATHS +++++++++++++++++++++
 
-console.log(Math);
-console.log(Ma)
+// console.log(Math);
+
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.67));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.2));
+
+// console.log(Math.max(78,32,43,56));
+// console.log(Math.min(78,32,43,56));
+
+// console.log(Math.sqrt(43.56));
+// console.log(Math.pow(89, 3));
+
+console.log(Math.random());
+console.log(Math.random()*10 + 1);
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10;
+const max = 20;
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+
+
+
+
+
+
