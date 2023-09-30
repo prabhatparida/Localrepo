316a6d6ac31e8e1280108ff1449304756029f9cc
prabhatparida <137138319+prabhatparida@users.noreply.github.com>
2023-09-30 02:44:52 +0000

    modified

--- 01_numsmaths.js
+++ 01_numsmaths.js
@@ -40,6 +40,7 @@ console.log(Math.floor(Math.random()*10) + 1);
 const min = 10;
 const max = 20;
 console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+console.log(Math.ceil(Math.random() * (max + min -1)) + max );
 
 
 
