d9d1c53a4d6c2872d3418ee32ab63f2d743a71ea
prabhatparida <137138319+prabhatparida@users.noreply.github.com>
2023-10-04 16:38:21 +0000

    new Arrays methods

--- 02_basics/02_arrays1.js
+++ 02_basics/02_arrays1.js
@@ -1,3 +1,39 @@
 const Heros = ['Shaktimaan', 'Arjun', 'RamChandra', 'Srikrishna'];
+const Dc_Heros = ['superman', 'flash', 'Batman'];
 
-console.log('A', Heros);
+// Heros.push(Dc_Heros);
+
+// console.log('A', Heros);
+
+// console.log('B', Heros[4]);
+
+// console.log('c', Heros[4][2]);
+
+// concat
+
+// const allHeros = Heros.concat(Dc_Heros);
+// console.log(allHeros);
+
+// Spread Operator
+
+// const all_newHeros = [...Heros, ...Dc_Heros];
+// console.log(all_newHeros);
+
+const newArray = [1,2,3,[4,5,6],7,[8,9,[10,11]]];
+const realArray = newArray.flat(Infinity);
+
+// console.log(realArray);
+
+console.log(Array.isArray('Prabhat'));
+console.log(Array.from('Prabhat'));
+console.log(Array.from({name:'Prabhat'})); // return empty
+
+let score = 125;
+let score1 = 23;
+let score2 = 67;
+
+console.log(Array.of(score,score2,score2));
+
+
+
+
