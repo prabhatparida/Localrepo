be986dd847ebae615a66f8cc6a93ef3f25244367
prabhatparida <137138319+prabhatparida@users.noreply.github.com>
2023-10-13 03:08:48 +0000

    While and do While Loop

---
+++ 06_iterations/Two.js
@@ -0,0 +1,21 @@
+// While & do While loop
+
+// let num = 0
+// while (num <= 12 ) {
+//     console.log(`The Number is ${num}`);
+//     num = num + 3;
+// }
+
+// let SuperHeros = ["Krishna", "Arjun", "Ramchandra", "Jagannath"];
+
+// let Heros = 0;
+// while (Heros < SuperHeros.length) {
+//     console.log(`Value is ${SuperHeros[Heros]}`);
+//     Heros = Heros + 1;
+// }
+
+let score = 1;
+do {
+    console.log(`Score is ${score}`);
+    score = score + 4;
+} while (score <= 20);
