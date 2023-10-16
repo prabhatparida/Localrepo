ead0ee84c5ed061f2c873e30b284ce340e629335
prabhatparida <137138319+prabhatparida@users.noreply.github.com>
2023-10-16 03:32:05 +0000

    higher order of loop

---
+++ 06_iterations/three.js
@@ -0,0 +1,37 @@
+// for of Loop
+
+// ["", "", ""]
+// [{},{},{}]
+
+// for of loop
+const stateName = ["Odisha", "Karnataka","Maharastra", "Goa", "TamilNadu"];
+
+// for (const name of stateName) {
+//     console.log(name);
+// }
+
+// const greetings = "Hello world!";
+// for (const greet of greetings) {
+//     console.log(`Each char is ${greet}`);
+// }
+
+//  Maps
+
+const map = new Map()
+map.set('IN', "India")
+map.set('USA', "United States Of America")
+map.set('AUS', "Australia")
+
+// console.log(map);
+
+for (const [key, value] of map) {
+    console.log(key, ':-', value);
+}
+
+
+const myObject = {
+    'game1' : 'NFS',
+    'game2' : 'Spiderman'
+}
+
+for
